6ae5505a56f1b15b2f1a94d9136eb1e22d0bafe2
roxxploxx <none>
2017-05-17 13:16:28 -0400

    * Fixed the PreFix->PostFix in HarmonyCompSlotLoadable.   Not a big thing at all, just a typo.
    * Refactored the grabbing of stats from a Slot. Now grabs all
      statModifiers of the slot for displaying and for incorporating
      values in StatExtension and now too StatWorker.StatOffsetFromGear.
    
      Created two new helper functions to do this:
      * CompSlotLoadable.CheckThingSlotsForStatAugment
      * CompSlotLoadable.GetSlots
    
      Created two new helper functions to do this:
      * CompSlotLoadable.CheckThingSlotsForStatAugment
      * CompSlotLoadable.GetSlots
    
      NOTE: I thought I would need to
      StatWorker.GearAffectsStat but that
      only works on ThingDefs and
      since slots have to be filled
      first, I
      think it might be ok for
      now? But, keep an eye on
      this.
    
    * Updated the Slottables to have their StatModifiers modified as parts, just like statModifiers in the statBase field.

--- Source/CompSlotLoadable/CompSlotLoadable.cs
+++ Source/CompSlotLoadable/CompSlotLoadable.cs
@@ -338,7 +338,7 @@ namespace CompSlotLoadable
                         }
                     }
                 }
-                
+
             }
             yield break;
         }
@@ -366,6 +366,19 @@ namespace CompSlotLoadable
                         {
                             if (slotBonus.Props.statModifiers != null && slotBonus.Props.statModifiers.Count > 0)
                             {
+                                s.AppendLine();
+                                s.AppendLine("StatModifiers".Translate() + ":");
+
+                                foreach (StatModifier mod in slotBonus.Props.statModifiers)
+                                {
+                                    float v = DetermineSlottableStatAugment(slot.SlotOccupant,mod.stat);
+                                    string modstring = 	mod.stat.ValueToString(v, ToStringNumberSense.Offset);
+//                                    Log.Message("Determined slot stat augment "+v+" and made string "+modstring);
+                                    s.AppendLine("\t" + mod.stat.LabelCap + " " + modstring);
+                                    //s.AppendLine("\t" + mod.stat.LabelCap + " " + mod.ToStringAsOffset);
+                                }
+                                /*
+                                //Log.Message("fix this to display statModifiers");
                                 List<StatModifier> statMods = slot.SlotOccupant.def.statBases.FindAll(
                                     (StatModifier z) => z.stat.category == StatCategoryDefOf.Weapon ||
                                                         z.stat.category == StatCategoryDefOf.EquippedStatOffsets);
@@ -378,6 +391,7 @@ namespace CompSlotLoadable
                                         s.AppendLine("\t" + mod.stat.LabelCap + " " + mod.ToStringAsOffset);
                                     }
                                 }
+                                */
                             }
                             DamageDef damageDef = slotBonus.Props.damageDef;
                             if (damageDef != null)
@@ -443,6 +457,77 @@ namespace CompSlotLoadable
                 return (CompProperties_SlotLoadable)this.props;
             }
         }
+
+
+        // Grab slots of the thing if they exists. Returns null if none
+        public static List<SlotLoadable> GetSlots(Thing someThing) {
+            List<SlotLoadable> retval = null;
+
+            ThingWithComps thingWithComps = someThing as ThingWithComps;
+            if ( thingWithComps != null ) {
+                ThingComp comp = thingWithComps.AllComps.FirstOrDefault((ThingComp x) => x is CompSlotLoadable);
+                if (comp != null)
+                {
+                    CompSlotLoadable compSlotLoadable = comp as CompSlotLoadable;
+
+                    if (compSlotLoadable.Slots != null && compSlotLoadable.Slots.Count > 0)
+                    { retval = compSlotLoadable.Slots; }
+                }
+            }
+
+            return retval;
+        }
+
+
+        // Get the thing's modificaiton to stat from it's slots
+        public static float CheckThingSlotsForStatAugment(Thing slottedThing, StatDef stat) {
+            float retval = 0.0f;
+            List<SlotLoadable> slots = CompSlotLoadable.GetSlots(slottedThing);
+
+            if ( slots != null ) {
+                foreach ( SlotLoadable slot in slots ) {
+                    if (!slot.IsEmpty())
+                    {
+                        Thing slottable = slot.SlotOccupant;
+                        retval += DetermineSlottableStatAugment(slottable ,stat);
+                    }
+                }
+            }
+            return retval;
+        }
+
+        public static float DetermineSlottableStatAugment(Thing slottable, StatDef stat) {
+            float retval =0.0f;
+            CompSlottedBonus slotBonus = slottable.TryGetComp<CompSlottedBonus>();
+            if (slotBonus != null)
+            {
+                if (slotBonus.Props != null)
+                {
+                    foreach ( StatModifier thisStat in slotBonus.Props.statModifiers ) {
+                        //Log.Message("Check for modding "+stat+"  against "+thisStat.stat);
+                        if ( thisStat.stat == stat ) {
+                            //Log.Message("adding in stat "+thisStat.stat+":"+thisStat.value+" to result "+retval);
+                            retval += thisStat.value;
+
+                            // apply stats parts from Slottable
+                            if (stat.parts != null)
+                            {
+                                StatRequest req = StatRequest.For(slottable);
+                                for (int i = 0; i < stat.parts.Count; i++)
+                                {
+                                    //Log.Message("adding in parts "+stat.parts[i]);
+                                    stat.parts[i].TransformValue(req, ref retval);
+                                }
+                                //Log.Message("added in parts of a stat for result "+retval);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return retval;
+        }
+
 
     }
 }
